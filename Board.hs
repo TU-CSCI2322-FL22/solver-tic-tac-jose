7391e1f1151f517a8cb1a638c86e90e6e01fad09
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2022-11-02 12:21:10 -0500

    after class

--- Board.hs
+++ Board.hs
@@ -1,88 +1,23 @@
 
 
 module Board where
-import Data.Type.Coercion (sym)
+-- import Data.Type.Coercion (sym)
+
+data Player = X | O deriving (Show)
+
+data Symbol = None | Claimed Player deriving (Show)
+
+type MoveSet = [Symbol]
 
 
-data Symbol = X | O | None deriving (Eq)
-
-instance Show Symbol where
-    show X = "X"
-    show O = "O"
-    show None = "_"
 
 type LBoard = [[Symbol]]
 
 type BBoard = [[LBoard]]
 
+
 type Location = (Int,Int)
 type Move = (Location, Symbol)
 
-
-evalList::[[Symbol]] -> [Symbol]
-evalList lst =
-    foldr (\ x -> (++) [compList x]) [] lst
-    where
-        compList lst = foldl1 (\a b -> if a == b then a else None) lst
-
-checkHoriz :: LBoard -> [Symbol]
-checkHoriz xs =
-    evalList xs
-
-ripFront :: [[a]] -> [a]
-ripFront lst =
-    let a = [(x,xs) | (x:xs) <- lst]
-        (b,_) = unzip a
-    in b
-
-ripBack :: [[a]] -> [[a]]
-ripBack lst =
-    let a = [(x,xs) | (x:xs) <- lst]
-        (_,b) = unzip a
-    in b
-
---gets the columns of a 2d list
-listVert :: [[a]] -> [[a]]
-listVert [] = []
-listVert lst
-    | not $ null a = ripFront lst : listVert (ripBack lst)
-    | otherwise = [ripFront lst]
-    where
-        a = ripFront $ ripBack lst
-
-checkVert :: LBoard -> [Symbol]
-checkVert lst = do
-    evalList (listVert lst)
-
-checkDiagonal :: [[Symbol]] -> [Symbol]
-checkDiagonal lst =
-    evalList $ [listDiagonal lst] ++ [listDiagonal (reverse lst)]
-
-listDiagonal :: [[a]] -> [a]
-listDiagonal [] = []
-listDiagonal (x:xs) =
-    let (a:as) = x
-    in [a] ++ listDiagonal (ripBack xs)
-
-
-checkLilBoard :: LBoard-> Symbol
-checkLilBoard board =
-    let a = checkHoriz board
-        b = checkVert board
-        diag = checkDiagonal board
-        c = a ++ b ++ diag
-        d = filter(\a -> a /= None) c
-    in if null d then None else foldl1 (\a b -> if a == b then a else error "multiple correct spaces") d
-
-checkBigBoard :: BBoard -> Symbol
-checkBigBoard board =
-    checkLilBoard [map checkLilBoard x | x <- board]
-
-
--- validMoves :: BBoard -> [Location]
--- validMoves = 
---     undefined
-
--- playMove :: BBoard -> Move -> BBoard
--- playMove =
---     undefined
+--LBoard = [(Int,Int)]
+--BBoard = (LBoard, LBoard, LBoard)
--- Game.hs
+++ Game.hs
@@ -13,42 +13,5 @@ module Game where
 import Board
 
 
--- | Returns if a game has been won
---
--- takes a BBoard
---
--- returns a symbol representing the winner
---
--- will return None if no winner yet
-checkWin :: BBoard -> Symbol
-checkWin board =
-    checkLilBoard [map checkLilBoard x | x <- board]
-
-
--- | Returns the valid moves from a given board
---
--- takes a BBoard
---
--- returns [Location]
-validMoves :: BBoard -> [Location]
-validMoves = 
-    undefined
-
--- | Returns the board after making a given move
---
--- takes a BBoard and Move
---
--- returns a BBoard
-playMove :: BBoard -> Move -> BBoard
-playMove =
-    undefined
-
--- | Returns if a Move is valid on a Board
-isValid :: BBoard -> Move -> Bool
-isValid =
-    undefined
-
-
-
-showBoard =
+winner board =
     undefined
