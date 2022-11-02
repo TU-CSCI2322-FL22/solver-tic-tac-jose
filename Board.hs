5c5636cab98508d4434e9c942493f02e73951b68
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2022-11-02 12:43:05 -0500

    documentation

--- Board.hs
+++ Board.hs
@@ -6,6 +6,9 @@ data Player = X | O deriving (Show)
 
 data Symbol = None | Claimed Player deriving (Show)
 
+-- | First int - The Board [0..8] that is being picked
+-- 
+-- Second int - the place on the smaller board [0..8] being picked
 type Location = (Int,Int)
 type Move = (Location, Player)
 
