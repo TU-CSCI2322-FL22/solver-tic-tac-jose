b59781cf903da0d88de799d438f3c6fbbc1083dd
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2022-11-04 12:55:59 -0500

    operational

--- Winner.hs
+++ Winner.hs
@@ -4,6 +4,28 @@ module Winner where
 import Board
 
 
+-- get list of locations for a player
+-- check if for every x in wins, if x is in the location data
+
+-- getLoc :: BBoard -> LBoard
+-- getLoc O  [(0,X),(3,X),(4,O)]
+getLoc :: Player -> LBoard -> [Integer]
+getLoc player board =
+    map (\(a,b) -> a) $ filter (\(a,b) -> b == player) board
+    
+
+correctV2 = [[z,z+1,z+2] | z <- [0,3,6]] ++ [[z,z+3,z+6] | z <- [0,1,2]] ++ [[0,4,8],[2,4,6]]
+
+findAll board = 
+    let locX = getLoc X board
+        locO = getLoc O board
+        correct = [[z,z+1,z+2] | z <- [0,3,6]] ++ [[z,z+3,z+6] | z <- [0,1,2]] ++ [[0,4,8],[2,4,6]]
+        x = any (==True) $ map (\a -> all (==True) [v `elem` locX | v <- a]) correct
+        o = any (==True) $ map (\a -> all (==True) [v `elem` locO | v <- a]) correct
+    in o
+        -- o = map 
+-- v = map (\a -> )
+
 composite :: BBoard -> LBoard
 composite board =
     let xData = [(x, X) | (x,passBoard) <- board, winnerBool X passBoard]
