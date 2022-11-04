8a041d5329bca60f41c07c1d3ce4edcc7e1919e3
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2022-11-04 13:10:19 -0500

    returning player

--- Winner.hs
+++ Winner.hs
@@ -9,20 +9,39 @@ import Board
 
 -- getLoc :: BBoard -> LBoard
 -- getLoc O  [(0,X),(3,X),(4,O)]
-getLoc :: Player -> LBoard -> [Integer]
-getLoc player board =
-    map (\(a,b) -> a) $ filter (\(a,b) -> b == player) board
-    
 
 correctV2 = [[z,z+1,z+2] | z <- [0,3,6]] ++ [[z,z+3,z+6] | z <- [0,1,2]] ++ [[0,4,8],[2,4,6]]
 
-findAll board = 
-    let locX = getLoc X board
-        locO = getLoc O board
-        correct = [[z,z+1,z+2] | z <- [0,3,6]] ++ [[z,z+3,z+6] | z <- [0,1,2]] ++ [[0,4,8],[2,4,6]]
-        x = any (==True) $ map (\a -> all (==True) [v `elem` locX | v <- a]) correct
-        o = any (==True) $ map (\a -> all (==True) [v `elem` locO | v <- a]) correct
-    in o
+testBoardX = [(y,[(x,X) | x <- [0..8]]) | y <- [0..8]]
+testBoardThree = [(0,[(0,X),(4,X),(8,X)]),(8,[(8,O)])]
+
+composition :: BBoard -> LBoard
+composition board =
+    map (\(a,b) -> (a, mapB b)) $ filter (\(a,b) -> if b == Nothing then False else True) $ map (\(a,b) -> (a, findAll b)) board
+    where
+        mapB :: Maybe Player -> Player
+        mapB player
+            | player == Just X = X
+            | player == Just O = O
+            | otherwise = error "this is supposed to be filtered out"
+
+        findAll :: LBoard -> Maybe Player
+        findAll board
+            | x && o = error "multiple wins? this should not be able to happen"
+            | x = Just X
+            | o = Just O
+            | otherwise = Nothing
+            where 
+                getLoc :: Player -> LBoard -> [Integer]
+                getLoc player board =
+                    map (\(a,b) -> a) $ filter (\(a,b) -> b == player) board
+                locX = getLoc X board
+                locO = getLoc O board
+                correct = [[z,z+1,z+2] | z <- [0,3,6]] ++ [[z,z+3,z+6] | z <- [0,1,2]] ++ [[0,4,8],[2,4,6]]
+                x = any (==True) $ map (\a -> all (==True) [v `elem` locX | v <- a]) correct
+                o = any (==True) $ map (\a -> all (==True) [v `elem` locO | v <- a]) correct
+
+
         -- o = map 
 -- v = map (\a -> )
 
