d43b34906933a59c9a3d28f86515225b0f49fca7
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2022-11-04 15:21:55 -0500

    Update Game.hs

--- Game.hs
+++ Game.hs
@@ -23,32 +23,28 @@ winner board
     | c == Nothing = Going
     | otherwise = error "this was not supposed to happen"
     where
-        c = composition board
-        composition :: BBoard -> Maybe Player
-        composition board =
-            findAll $ map (\(a,b) -> (a, mapB b)) $ filter (\(a,b) -> b /= Nothing) $ map (\(a,b) -> (a, findAll b)) board
-            where
-                mapB :: Maybe Player -> Player
-                mapB player
-                    | player == Just X = X
-                    | player == Just O = O
-                    | otherwise = error "this is supposed to be filtered out"
+        c = findAll $ map (\(a,b) -> (a, mapB b)) $ filter (\(a,b) -> b /= Nothing) $ map (\(a,b) -> (a, findAll b)) board
+        mapB :: Maybe Player -> Player
+        mapB player
+            | player == Just X = X
+            | player == Just O = O
+            | otherwise = error "this is supposed to be filtered out"
 
-                findAll :: LBoard -> Maybe Player
-                findAll board
-                    | x && o = error "multiple wins? this should not be able to happen"
-                    | x = Just X
-                    | o = Just O
-                    | otherwise = Nothing
-                    where
-                        getLoc :: Player -> LBoard -> [Integer]
-                        getLoc player board =
-                            map (\(a,b) -> a) $ filter (\(a,b) -> b == player) board
-                        locX = getLoc X board
-                        locO = getLoc O board
-                        correct = [[z,z+1,z+2] | z <- [0,3,6]] ++ [[z,z+3,z+6] | z <- [0,1,2]] ++ [[0,4,8],[2,4,6]]
-                        x = any ((==True) . (\a -> all (==True) [v `elem` locX | v <- a])) correct
-                        o = any ((==True) . (\a -> all (==True) [v `elem` locO | v <- a])) correct
+        findAll :: LBoard -> Maybe Player
+        findAll board
+            | x && o = error "multiple wins? this should not be able to happen"
+            | x = Just X
+            | o = Just O
+            | otherwise = Nothing
+            where
+                getLoc :: Player -> LBoard -> [Integer]
+                getLoc player board =
+                    map (\(a,b) -> a) $ filter (\(a,b) -> b == player) board
+                locX = getLoc X board
+                locO = getLoc O board
+                correct = [[z,z+1,z+2] | z <- [0,3,6]] ++ [[z,z+3,z+6] | z <- [0,1,2]] ++ [[0,4,8],[2,4,6]]
+                x = any ((==True) . (\a -> all (==True) [v `elem` locX | v <- a])) correct
+                o = any ((==True) . (\a -> all (==True) [v `elem` locO | v <- a])) correct
 
 
 -- | returns a board with a move made on it
