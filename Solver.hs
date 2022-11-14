9ebe60865e2356f8b8cab32bb48de1788ff866d7
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2022-11-14 13:45:46 -0600

    works on 7/8

--- Solver.hs
+++ Solver.hs
@@ -1,7 +1,7 @@
 
 module Solver where
 
-import Board 
+import Board
 import Game
 import Data.Maybe
 
@@ -12,8 +12,9 @@ getOtherP::Player->Player
 getOtherP p
         | p == X = O
         | p == O = X
+
 bench :: Game -> Integer
-bench (board, turn) 
+bench (board, turn)
                 | winner (board,turn) == Done (Win X) = 1
                 | winner (board,turn) == Done (Win O) = -1
                 | winner (board,turn) == Done Tie = 0
@@ -21,11 +22,39 @@ bench (board, turn)
                     where newGames = [(nBoard, ((getOtherP) (fst turn), fst m)) | nBoard<-nBoards, m <- vMoves]
                           nBoards = catMaybes [makeMove (board,turn) m | m<-vMoves]
                           vMoves = legalMoves (board,turn)
+
+compress :: [Outcome] -> Outcome
+compress lst
+    | Win X `elem` lst = Win X
+    | Win O `elem` lst = Win O
+    | Tie `elem` lst = Tie
+    -- | otherwise = Going
+
+-- Write  a function "who will win" that takes a Game and returns an Outcome. 
 whoWins :: Game -> Outcome
 whoWins game =
-            | bench game > 0 = Win X
-            | bench game < 0 = Win O
-            | otherwise = Tie
+    case winner game of
+        Done (Win X) -> Win X
+        Done (Win O) -> Win O
+        Done Tie -> Tie
+        _ -> fool
+    where
+        moves = legalMoves game
+        other = getOtherP (fst $ snd game)
+        -- newGames = catMaybes $ map (makeMove game) moves
+        getGame = [(perfectMove (fst game) (a,b) (fst $ snd game), (other, a)) | (a,b) <- moves]
+        -- tru = [(a,b) | (a,b) <- getGame, a /= Nothing]
+        -- tru = mapMaybe (\(a,b) -> b) getGame
+        -- post = catMaybes getGame
+        recur = map whoWins getGame
+        fool = compress recur
+        
+        -- comp = compress newGames
+
+
+            -- | bench game > 0 = Win X
+            -- | bench game < 0 = Win O
+            -- | otherwise = Tie
 
 -- Then write a function "best move" that takes a Game and return the best Move.
 bestMove :: Game -> Move
