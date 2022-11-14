ab236ab8a1a2f0307b547d59b64e906b065afb9d
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2022-11-14 16:02:25 -0600

    updated testing

--- Testing.hs
+++ Testing.hs
@@ -142,11 +142,11 @@ milestoneTwo =
             it "top left tie" $ do
                 bestMove ([(0,[(0,X),(1,X),(2,O),(3,O),(4,O),(5,X),(6,X),(7,X),(8,O)]),(6,[(0,X),(1,X),(2,X)]),(7,[(0,X),(2,X),(6,X),(8,X)]), (8,[(0,X),(2,X),(6,X),(8,X)])],(X,8)) `shouldSatisfy` (\x -> x `elem` [(8,1),(8,3),(8,4),(8,5),(8,7)])
             it "three corners" $ do
-                bestMove (fillBoardDiagonalX ++ [(8,[(0,X),(2,X),(8,X)])], (X,8)) `shouldSatisfy` (\x -> x `elem` [(8,1),(8,5)])
+                bestMove (fillBoardDiagonalX ++ [(8,[(0,X),(2,X),(8,X)])], (X,8)) `shouldSatisfy` (\x -> x `elem` [(8,1),(8,4),(8,5)])
             it "x corner start" $ do
-                bestMove (fillBoardDiagonalX ++ [(8,[(0,X),(7,O)])], (X,8)) `shouldSatisfy` (\x -> x `elem` [(8,2),(8,6)])
+                bestMove (fillBoardDiagonalX ++ [(8,[(0,X),(7,O)])], (X,8)) `shouldSatisfy` (\x -> x `elem` [(8,2),(8,2),(8,4),(8,6)])
             it "force game middle" $ do
-                bestMove (fillBoardDiagonalX ++ [(8,[(0,X),(4,O),(8,X)])], (X,8)) `shouldSatisfy` (\x -> x `elem` [(8,2),(8,6)])
+                bestMove (fillBoardDiagonalX ++ [(8,[(0,X),(4,O),(8,X)])], (X,8)) `shouldSatisfy` (\x -> x `elem` [(8,1),(8,2),(8,3),(8,4),(8,5),(8,6),(8,7)])
             it "win deep" $ do
                 bestMove (tieTop ++ [(6,[(0,X),(1,X),(2,X)])] ++ [(7,[(0,X),(8,X)])] ++  [(8,[(0,X),(4,O),(8,X)])],(X,8)) `shouldSatisfy` (\x -> x `elem` [(8,1),(8,2),(8,3),(8,5),(8,6),(8,7)]) 
             
